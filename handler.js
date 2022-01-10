8eb4e29d030ca305ed4dcfea3ab65e9a366eb6dd
Samuel Gabriel <splendy5@gmail.com>
2022-01-10 21:24:50 +0800

    Add fixes. Update response

--- handler.js
+++ handler.js
@@ -13,82 +13,95 @@ module.exports.run = (event, context, callback) => {
   const START_UP_TAG = "AUTO-START";
   const SHUT_DOWN_TAG = "AUTO-STOP";
 
+  // Result of operation
+  var result = [];
+
   // Get List of RDS Instances
   rds.describeDBInstances(null, function (rdserr, rdsdata) {
 
-    if (rdserr) callback(rdsdata, null)
-    if (!rdsdata) callback(new Error('Failed to get Databases'), null)
+    // An error occurred
+    if (rdserr) return callback(rdserr, null)
 
     // Loop through instance list
     rdsdata.DBInstances.forEach(function (dbInstance) {
+
       var rdstagParams = {
         ResourceName: dbInstance.DBInstanceArn
       }
 
-      // Get List of tags for instance
-      rds.listTagsForResource(rdstagParams, function (tagerr, tagdata) {
+      // If status is available
+      if (dbInstance.DBInstanceStatus === 'available' || dbInstance.DBInstanceStatus === 'stopped') {
 
-        var toStartup = false;
-        var toShutdown = false
+        // Get List of tags for instance
+        rds.listTagsForResource(rdstagParams, function (tagerr, tagdata) {
 
-        // An error occurred
-        callback({
-          error: tagerr,
-          stack: tagerr.stack
-        }, null)
+          var toStartup = false;
+          var toShutdown = false
+          var response = {
+            autoStart: false,
+            autoStop: false
+          };
 
-        // Get and loop through tag list to find required tags
-        var tags = tagdata.TagList || []
-        tags.forEach(function (tag) {
+          // An error occurred
+          if (tagerr) return callback(tagerr, null);
 
-          // Check if Start up tag exist
-          if (tag.Key && tag.Key.toUpperCase() === START_UP_TAG) {
-            if (tag.Value !== '0' && tag.Value !== 'false'  && tag.Value !== false) {
-              toStartup = start;
+          // Get and loop through tag list to find required tags
+          var tags = tagdata.TagList || []
+          tags.forEach(function (tag) {
+
+            // Check if Start up tag exist
+            if (tag.Key && tag.Key.toUpperCase() === START_UP_TAG) {
+              if (tag.Value !== '0' && tag.Value !== 'false' && tag.Value !== false) {
+                toStartup = start;
+              }
             }
+
+            // Check if Shut down tag exist
+            if (stop && tag.Key && tag.Key.toUpperCase() === SHUT_DOWN_TAG) {
+              if (tag.Value !== '0' && tag.Value !== 'false' && tag.Value !== false) {
+                toShutdown = stop;
+              }
+            }
+          })
+
+
+          // Process Start request
+          if (toStartup) {
+            var startparams = {
+              DBInstanceIdentifier: dbInstance.DBInstanceIdentifier /* required */
+            }
+            rds.startDBInstance(startparams, function (starterr, startdata) {
+              // An error occurred
+              if (starterr) return callback(starterr, null);
+              response.autoStart = true;
+              response.autoStartData = startdata;
+            })
+          } else {
+            response.autoStart = false;
           }
 
-          // Check if Shut down tag exist
-          if (stop && tag.Key && tag.Key.toUpperCase() === SHUT_DOWN_TAG) {
-            if (tag.Value !== '0' && tag.Value !== 'false'  && tag.Value !== false) {
-              toShutdown = stop;
+          // Process Stop request
+          if (toShutdown) {
+            var shutdownparams = {
+              DBInstanceIdentifier: dbInstance.DBInstanceIdentifier
             }
+            rds.stopDBInstance(shutdownparams, function (stoperr, stopdata) {
+              // An error occurred
+              if (stoperr) return callback(stoperr, null);
+              response.autoStop = true;
+              response.autoStopData = stopdata;
+            })
+          } else {
+            response.autoStop = false;
           }
+
+          // Add response for id
+          result.push({ id: dbInstance.DBInstanceIdentifier, result: response});
         })
-
-
-        // Process Start request
-        if (toStartup) {
-          var startparams = {
-            DBInstanceIdentifier: dbInstance.DBInstanceIdentifier /* required */
-          }
-          rds.startDBInstance(startparams, function (starterr, startdata) {
-            if (starterr)
-              callback(starterr, null)
-            else
-              callback(null, startdata)
-          })
-        }
-        else {
-          console.log('Nothing to start');
-        }
-
-        // Process Stop request
-        if (toShutdown) {
-          var shutdownparams = {
-            DBInstanceIdentifier: dbInstance.DBInstanceIdentifier
-          }
-          rds.stopDBInstance(shutdownparams, function (stoperr, stopdata) {
-            if (stoperr)
-              callback(stoperr, null)
-            else
-              callback(null, stopdata)
-          })
-        }
-        else {
-          console.log('Nothing to stop');
-        }
-      })
+      }
     })
   })
+
+  // Send response
+  callback(null, result);
 }
